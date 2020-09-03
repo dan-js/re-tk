3acf9703a082f96bfee92e8ad87556edf2bccaed
Dan Nash <dan@nuuji.tech>
2020-09-03 21:25:39 +0100

    v0.1.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
+/* eslint-env node */
 const { react } = require("@nuuji/config");
+const { sync: globSync } = require("glob");
 
 const config = react.rollup({ name: "reactsan" });
 
 config.preserveModules = true;
 
+console.log(globSync("src/*/index.js"));
+
 delete config.output[0].file;
 delete config.output[1];
 config.output[0].dir = "lib";
-config.input = ["src/index.js", "src/combine/index.js"];
-
-console.log(config);
+config.input = ["src/index.js", ...globSync("src/*/index.js")];
 
 // TODO: investigate this
 // config.output[0].sourcemap = true;
